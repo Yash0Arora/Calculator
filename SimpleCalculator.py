165aabffe02a29328c9fdb5838c2c532e22bba32
Yash0Arora <85996197+Yash0Arora@users.noreply.github.com>
2021-06-17 13:34:31 +0530

    Add files via upload

---
+++ SimpleCalculator.py
@@ -0,0 +1,32 @@
+sign = input("What Do You Want Me To Do ? (+ , - , * , /) ")
+if sign == "+" :
+
+    first_number = input("First Number =  " )
+    second_number = input("Second Number =  ")
+    sum = float(first_number) + float(second_number)
+    print("Sum = " + str(sum))
+
+if sign == "-" : 
+    
+    first_number = input("First Number =  " )
+    second_number = input("Second Number =  ")
+    sum = float(first_number) - float(second_number)
+    print("Sum = " + str(sum))
+
+if sign == "*" : 
+
+    
+    first_number = input("First Number =  " )
+    second_number = input("Second Number =  ")
+    sum = float(first_number) * float(second_number)
+    print("Sum = " + str(sum))
+
+if sign == "/" :
+
+    first_number = input("First Number =  " )
+    second_number = input("Second Number =  ")
+    sum = float(first_number) / float(second_number)
+    print("Sum = " + str(sum))
+
+
+
