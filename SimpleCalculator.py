6339b15229dfd162f960fb777012691d4737f385
TECHISHANT <74040166+techishant@users.noreply.github.com>
2021-07-01 20:22:14 +0530

    Fixed bugs (full info in description)
    1) Fixing bug in which sum was written for alll evalutation(added product, diffrence, quotient)
    
    2)adding elif instead of if (also else if user type something not available) gives user a sorry msg
    
    3)now writing sqrt in any form gives square root value
    
    4) changed position of this statement 
    "sqrt" to Find Square Root

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -1,5 +1,5 @@
 import math
-sign = input('What Do You Want Me To Do ? \n"+" for Addition \n"-" for Subtarction\n"*" for Multiplication \n"/" for Division \n"E" to Evaluate \n"sqrt" to Find Square Root \n\n    "?" for Help and operators to use in EVALUATE\n ')
+sign = input('What Do You Want Me To Do ? \n"+" for Addition \n"-" for Subtarction\n"*" for Multiplication \n"/" for Division \n"sqrt" to Find Square Root  \n"E" to Evaluate \n\n    "?" for Help and operators to use in EVALUATE\n ')
 
 if (sign == "?") :
     help_file = open("helpText.txt", "r")
@@ -9,7 +9,7 @@ if (sign == "?") :
 
 # To Add
 
-if sign == "+" :
+elif (sign == "+") :
 
     first_number = input("First Number =  " )
     second_number = input("Second Number =  ")
@@ -18,34 +18,34 @@ if sign == "+" :
 
 # To subtract
 
-if sign == "-" : 
+elif (sign == "-") : 
     
     first_number = input("First Number =  " )
     second_number = input("Second Number =  ")
-    sum = float(first_number) - float(second_number)
-    print("Sum = " + str(sum))
+    diffrence = float(first_number) - float(second_number)
+    print("Difference = " + str(diffrence))
 
 # To multiply
 
-if sign == "*" : 
+elif (sign == "*") : 
 
     first_number = input("First Number =  " )
     second_number = input("Second Number =  ")
-    sum = float(first_number) * float(second_number)
-    print("Sum = " + str(sum))
+    product = float(first_number) * float(second_number)
+    print("Product = " + str(product))
 
 # To Divide
 
-if sign == "/" :
+elif (sign == "/") :
 
     first_number = input("First Number =  " )
     second_number = input("Second Number =  ")
-    sum = float(first_number) / float(second_number)
-    print("Sum = " + str(sum))
+    quotient = float(first_number) / float(second_number)
+    print("Answer = " + str(quotient))
 
 # To evaluate
 
-if (sign == "E") :
+elif (sign.upper() == "E") :
 
     print("Give your expression")
     expre = input("")
@@ -53,6 +53,11 @@ if (sign == "E") :
 
 #to find square root
 
-if sign == 'Sqrt':
+elif (sign.upper() == 'SQRT'):
      number = input("Of What Number Should I find Square Root Of? ")
      print(math.sqrt(int(number)))
+
+# if nothing is typed
+
+else:
+    print("Sorry, we coud'nt help you")
